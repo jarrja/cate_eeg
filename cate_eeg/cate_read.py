9af076e2254046af7d6c14de868e40d27a399ae3
Panuwat Janwattanapong <jarr@Panuwats-MacBook-Pro-2.local>
2017-06-12 13:46:59 -0400

    Add edf_to_raw function

--- cate_eeg/cate_read.py
+++ cate_eeg/cate_read.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import mne
+import warnings
 
 def text_to_raw(file_name,location,sfreq,col_remove = [],montage_type = 'standard_1020'):
     #Read EEG data from text file
@@ -25,8 +26,25 @@ def text_to_raw(file_name,location,sfreq,col_remove = [],montage_type = 'standar
     #Set Montage
     montage = mne.channels.read_montage(montage_type)
     raw.set_montage(montage)
-    
+    #Return raw MNE file
     return raw
+
+def edf_to_raw(edf_file_name,chan_select = [],montage_type = 'standard_1020'):
+    #If no channel is selcted, assign the default channel to the selection
+    if not chan_select:
+        warnings.warn('Empty selection of channel, Default channels are selected')
+        chan_select = [u'C3', u'C4', u'O1', u'O2', u'Cz', u'F3', u'F4', u'F7', u'F8', u'Fz', u'Fp1', u'Fp2', u'P3', u'P4', u'Pz', u'T3', u'T4', u'T5', u'T6']
+    #Read edf file to raw
+    raw = mne.io.read_raw_edf(edf_file_name,preload=True)
+    #Apply selection of channel
+    raw.pick_channels(chan_select)
+    #Set montage
+    montage = mne.channels.read_montage(montage_type)
+    raw.set_montage(montage)
+    #Return raw MNE file
+    return raw
+
+
 
 if __name__ == '__main__':
     print "CATE_EEG"
