b1b7d2cbc686bd106f0f64cb885dc1e93a36657a
Jarr CATE_PC <pjanw001@fiu.edu>
2017-07-06 14:23:39 -0400

    test for branching22

--- cate_eeg/cate_fc.py
+++ cate_eeg/cate_fc.py
@@ -6,6 +6,7 @@ import os
 # Workstation between windows and OSX
 FUNC_DIR = '/Users/jarr/Dropbox/Research/CATE_EEG_MASTER/cate_eeg'
 test = 'windows'
+test2 = 'Helloworld'
 
 def connection_count(connectivity_matrix, mask_name, threshold):
     """
